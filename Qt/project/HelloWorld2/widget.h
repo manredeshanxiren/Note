b607d5de29a85f518f376aa509343f792ccd7f10
manredeshanxiren <1448554339@qq.com>
2024-05-03 20:40:17 +0800

    hello

---
+++ Qt/project/HelloWorld2/widget.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    void handleClick();
+
+private:
+    Ui::Widget *ui;
+};
+
